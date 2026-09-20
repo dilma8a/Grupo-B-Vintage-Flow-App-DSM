4cd648d900ba9c3874e8e8683851a76099e361cc
dilma8a <dilmaesmeralda8a@gmail.com>
2026-09-20 11:24:24 -0600

    Se agrega configuracion UTF_8 para correccion de tildes

--- Main.kt
+++ Main.kt
@@ -1,4 +1,10 @@
+import java.io.PrintStream
+import java.nio.charset.StandardCharsets
+
 fun main() {
+    System.setOut(PrintStream(System.out, true, StandardCharsets.UTF_8.name()))
+    System.setErr(PrintStream(System.err, true, StandardCharsets.UTF_8.name()))
+
     val inventarioPrincipal = mutableListOf<Prenda>()
     val gestorBase = ModuloGestionPrendas(inventarioPrincipal)
     val gestorValidado = GestorPrendasValidado(gestorBase)
