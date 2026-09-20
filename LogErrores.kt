1ac21a20545e67f330b72059c0c12dc502216bee
dilma8a <dilmaesmeralda8a@gmail.com>
2026-09-20 11:01:06 -0600

    Se corrige formato de fecha en log de errores

--- LogErrores.kt
+++ LogErrores.kt
@@ -1,8 +1,13 @@
 import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.logging.FileHandler
+import java.util.logging.Formatter
 import java.util.logging.Level
+import java.util.logging.LogRecord
 import java.util.logging.Logger
-import java.util.logging.SimpleFormatter
 
 object LogErrores {
     private val logger: Logger = Logger.getLogger("VintageFlow")
@@ -16,7 +21,26 @@ object LogErrores {
             5,
             true
         )
-        handler.formatter = SimpleFormatter()
+        handler.formatter = object : Formatter() {
+            override fun format(record: LogRecord): String {
+                val fecha = SimpleDateFormat("yyyy/MM/dd").format(Date(record.millis))
+                val hora = SimpleDateFormat("hh:mm:ss a").format(Date(record.millis))
+                val builder = StringBuilder()
+                builder.append("[$fecha $hora] ")
+                builder.append("${record.level}: ")
+                builder.append(record.message)
+                builder.append(System.lineSeparator())
+
+                if (record.thrown != null) {
+                    val sw = StringWriter()
+                    val pw = PrintWriter(sw)
+                    record.thrown.printStackTrace(pw)
+                    builder.append(sw.toString())
+                }
+
+                return builder.toString()
+            }
+        }
 
         logger.addHandler(handler)
         logger.useParentHandlers = false
